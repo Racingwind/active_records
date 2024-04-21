386e8dd9b266675998319f379040a7cec807fa0a
Racingwind <louis.k.hu@gmail.com>
2024-04-20 23:42:30 -0700

    completed the requirement

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Louis", last_name: "Hu", hourly_rate: 30)
+@store2.employees.create(first_name: "Steph", last_name: "Hu", hourly_rate: 40)
+@store1.employees.create(first_name: "Teresa", last_name: "Hu", hourly_rate: 50)
+
+@employees = Employee.all
+
+@employees.each do |employee|
+  puts "#{employee.first_name} #{employee.last_name} makes #{employee.hourly_rate} an hour"
+end
