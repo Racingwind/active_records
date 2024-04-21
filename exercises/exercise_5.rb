9678b7a2fd3b34d19a4d52b7174d1fc09697938b
Racingwind <louis.k.hu@gmail.com>
2024-04-20 23:35:23 -0700

    compeleted the requirement

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_rev = Store.sum(:annual_revenue)
+puts @total_rev
+
+@avg_rev = Store.average(:annual_revenue)
+puts @avg_rev
+
+puts Store.where("annual_revenue >= ?", 1_000_000).count
